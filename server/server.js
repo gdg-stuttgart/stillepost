eed22371b2a5a8e1dac0c9c7e0757b38abc153c1
Tobias Domhan <tdomhan@gmail.com>
2010-08-13 21:09:05 +0200

    server hinzu

---
+++ server/server.js
@@ -0,0 +1,11 @@
+var net = require('net');
+net.createServer(function (socket) {
+  socket.setEncoding("utf8");
+  socket.write("Echo server\r\n");
+  socket.on("data", function (data) {
+    socket.write(data);
+  });
+  socket.on("end", function () {
+    socket.end();
+  });
+}).listen(8124, "127.0.0.1");
