a6ddaeacd6a91e31eebb8de4bb9a1d1081a57541
thobach <info@thobach.de>
2010-08-14 15:24:47 +0200

    js eingebunden

--- client/js/client.js
+++ client/js/client.js
@@ -1,5 +1,15 @@
 function create_game(){
-	var name = document.getElementById('name').value;
+	var game = document.getElementById('game').value;
 	var player = document.getElementById('player').value;
-	
+	var json = "{ \"type\": \"create_game\", \"arguments\": { \"game\": \""+game+"\", \"player\": \""+player+"\" } }";
+	alert(json);
+	socket.send(json);
+}
+
+function join_game(){
+	var game = document.getElementById('game').value;
+	var player = document.getElementById('player').value;
+	var json = "{ \"type\": \"join_game\", \"arguments\": { \"game\": \""+game+"\", \"player\": \""+player+"\" } }";
+	alert(json);
+	socket.send(json);
 }
